d77edae2b8d6bfe54089d0b6b5dbc150ad70468f
Armando Di Cianno <armando@dicianno.org>
2015-07-31 11:07:20 -0400

    Another libxml exposed type

--- KissXML/Categories/NSString+DDXML.h
+++ KissXML/Categories/NSString+DDXML.h
@@ -1,12 +1,14 @@
 
 #import <Foundation/Foundation.h>
 
+typedef unsigned char ddXmlChar;
+
 @interface NSString (DDXML)
 
 /**
  * xmlChar - A basic replacement for char, a byte in a UTF-8 encoded string.
 **/
-- (const xmlChar *)xmlChar;
+- (const ddXmlChar *)xmlChar;
 
 - (NSString *)stringByTrimming;
 
