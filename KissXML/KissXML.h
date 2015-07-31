895d5246c05800006e226e8a5e44f012039e458f
Armando Di Cianno <armando@dicianno.org>
2015-07-31 13:28:34 -0400

    Complete umbrella header

--- KissXML/KissXML.h
+++ KissXML/KissXML.h
@@ -17,3 +17,8 @@ FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <KissXML/PublicHeader.h>
 
 #import <KissXML/DDXML.h>
+#import <KissXML/DDXMLNode.h>
+#import <KissXML/DDXMLElement.h>
+#import <KissXML/DDXMLDocument.h>
+#import <KissXML/DDXMLElementAdditions.h>
+#import <KissXML/NSString+DDXML.h>
