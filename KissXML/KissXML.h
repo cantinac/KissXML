94221bdf273eb52ad695ba490fca9cbfe7b39eaa
Armando Di Cianno <armando@dicianno.org>
2015-07-31 12:55:27 -0400

    Restructure proj for app and frameworks (osx,ios)

---
+++ KissXML/KissXML.h
@@ -0,0 +1,19 @@
+//
+//  KissXML.h
+//  KissXML
+//
+//  Created by Armando Di Cianno on 7/31/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KissXML.
+FOUNDATION_EXPORT double KissXMLVersionNumber;
+
+//! Project version string for KissXML.
+FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KissXML/PublicHeader.h>
+
+#import <KissXML/DDXML.h>
